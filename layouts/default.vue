47ef2ac62d670bc84d2b4b69394b580d45d11ae0
Oliver Sader <thunfisch987@gmail.com>
2023-09-30 01:34:39 +0200

    🖥️ wip: pwa install button

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,5 +2,12 @@
 	<div>
 		<MaguroNav />
 		<slot />
+		<ClientOnly>
+			<UButton @click="$pwa.install">Install, i guess</UButton>
+		</ClientOnly>
 	</div>
 </template>
+
+<script setup>
+const { $pwa } = useNuxtApp();
+</script>
