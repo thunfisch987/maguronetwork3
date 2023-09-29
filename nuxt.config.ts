7b39b91b972ba47fe8a1150f10a8dc228d422282
Oliver Sader <thunfisch987@gmail.com>
2023-09-30 01:31:28 +0200

    🖥️ wip: add pwa config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,4 +25,45 @@ export default defineNuxtConfig({
 		preference: 'dark',
 		fallback: 'dark',
 	},
+	pwa: {
+		client: {
+			installPrompt: true,
+		},
+		registerType: 'autoUpdate',
+		includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+		manifest: {
+			name: 'MaguroNetwork v3',
+			short_name: 'MaguroNetwork3',
+			theme_color: '#ffffff',
+			description: 'Magu Magu Netw',
+			icons: [
+				{
+					src: 'pwa-64x64.png',
+					sizes: '64x64',
+					type: 'image/png',
+				},
+				{
+					src: 'pwa-192x192.png',
+					sizes: '192x192',
+					type: 'image/png',
+				},
+				{
+					src: 'pwa-512x512.png',
+					sizes: '512x512',
+					type: 'image/png',
+					purpose: 'any',
+				},
+				{
+					src: 'maskable-icon-512x512.png',
+					sizes: '512x512',
+					type: 'image/png',
+					purpose: 'any maskable',
+				},
+			],
+		},
+		workbox: {
+			navigateFallback: '/',
+			globPatterns: ['**/*.{js,ts,css,html,png,svg,ico}'],
+		},
+	},
 });
