6edf493494d3babb5a600e2b427c480b310b2cf2
Oliver Sader <thunfisch987@gmail.com>
2023-10-02 22:49:10 +0200

    :wrench: default head options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,8 @@ export default defineNuxtConfig({
 	},
 	app: {
 		head: {
+			charset: 'utf-8',
+			viewport: 'width=device-width, initial-scale=1',
 			title: 'MaguroNetwork',
 			htmlAttrs: {
 				lang: 'en',
