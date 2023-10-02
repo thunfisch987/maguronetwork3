696f7db07008304d6757c806efa8bf8f7bb7e417
Oliver Sader <thunfisch987@gmail.com>
2023-10-02 23:07:08 +0200

    :sparkles: (nav) add possibility for child element

--- components/MaguroNav.vue
+++ components/MaguroNav.vue
@@ -1,12 +1,23 @@
 <template>
 	<UVerticalNavigation :links="myLinks">
+		<template #default="{ link, to }">
+			<div class="relative text-left w-full">
+				<div v-if="link.label" class="mb-2">
+					{{ link.label }}
+				</div>
+				<UVerticalNavigation
+					v-if="link.children"
+					:links="link.children"
+				></UVerticalNavigation>
+			</div>
+		</template>
 	</UVerticalNavigation>
 </template>
 
 <script setup lang="ts">
 import { VerticalNavigationLink } from '#ui/types/vertical-navigation';
 
-const myLinks: VerticalNavigationLink[] = [
+const myLinks: MyVerticalNavigationLink[] = [
 	{ label: 'MaguroNetwork', to: { name: 'index' } },
 	{ label: 'Akela', to: { name: 'Akela' }, icon: 'i-heroicons-dog' },
 	{ label: 'EnergyDrinkWiki', to: { name: 'EnergyDrinkWiki' } },
@@ -16,7 +27,12 @@ const myLinks: VerticalNavigationLink[] = [
 	{
 		label: 'Razer4Ever',
 		to: { name: 'Razer4Ever' },
+		children: [{ label: 'Razer4Ever' }],
 	},
 	{ label: 'RocketGame', href: 'https://rg.littlebitgay.de' },
 ];
+
+interface MyVerticalNavigationLink extends VerticalNavigationLink {
+	children?: VerticalNavigationLink[];
+}
 </script>
