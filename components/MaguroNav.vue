0497001dd8fbfc8f46aadffa62f0e36f94389a00
Oliver Sader <thunfisch987@gmail.com>
2023-10-02 23:03:47 +0200

    :sparkles: add nav

--- components/MaguroNav.vue
+++ components/MaguroNav.vue
@@ -1,3 +1,22 @@
 <template>
-	<div></div>
+	<UVerticalNavigation :links="myLinks">
+	</UVerticalNavigation>
 </template>
+
+<script setup lang="ts">
+import { VerticalNavigationLink } from '#ui/types/vertical-navigation';
+
+const myLinks: VerticalNavigationLink[] = [
+	{ label: 'MaguroNetwork', to: { name: 'index' } },
+	{ label: 'Akela', to: { name: 'Akela' }, icon: 'i-heroicons-dog' },
+	{ label: 'EnergyDrinkWiki', to: { name: 'EnergyDrinkWiki' } },
+	{ label: 'RandomMemes', to: { name: 'RandomMemes' } },
+	{ label: 'Vegalou', to: { name: 'Vegalou' } },
+	{ label: 'Weebsite', to: { name: 'Weebsite' } },
+	{
+		label: 'Razer4Ever',
+		to: { name: 'Razer4Ever' },
+	},
+	{ label: 'RocketGame', href: 'https://rg.littlebitgay.de' },
+];
+</script>
