ce49a26f4b545284bce33e541bc92cd5c1732561
Oliver Sader <thunfisch987@gmail.com>
2023-10-02 22:44:24 +0200

    :art: change primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,22 @@
 import type { Config } from 'tailwindcss';
 export default <Partial<Config>>{
+	theme: {
+		extend: {
+			colors: {
+				myprimary: {
+					'50': '#eeefff',
+					'100': '#e0e1ff',
+					'200': '#c9c8fd',
+					'300': '#a8a6fb',
+					'400': '#9083f6',
+					'500': '#7e65ef',
+					'600': '#7048e3',
+					'700': '#623ac8',
+					'800': '#4e319f',
+					'900': '#422f80',
+					'950': '#281c4a',
+				},
+			},
+		},
+	},
 };
