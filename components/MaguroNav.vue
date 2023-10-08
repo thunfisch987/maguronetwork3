a1108d62d5a6b996f8c941e1c49814bc97304c76
Oliver Sader <thunfisch987@gmail.com>
2023-10-09 01:48:12 +0200

    :art: (navbar) add icons

--- components/MaguroNav.vue
+++ components/MaguroNav.vue
@@ -6,15 +6,26 @@
 import { VerticalNavigationLink } from '#ui/types/vertical-navigation';
 
 const myLinks: VerticalNavigationLink[] = [
-	{ label: 'MaguroNetwork', to: { name: 'index' } },
-	{ label: 'Akela', to: { name: 'Akela' }, icon: 'i-heroicons-dog' },
-	{ label: 'EnergyDrinkWiki', to: { name: 'EnergyDrinkWiki' } },
-	{ label: 'RandomMemes', to: { name: 'RandomMemes' } },
+	{
+		label: 'MaguroNetwork',
+		to: { name: 'index' },
+	},
+	{
+		label: 'Akela',
+		to: { name: 'Akela' },
+		icon: 'i-fluent-emoji-high-contrast-dog-face',
+	},
+	{
+		label: 'EnergyDrinkWiki',
+		to: { name: 'EnergyDrinkWiki' },
+		icon: 'i-game-icons-soda-can',
+	},
 	{ label: 'Vegalou', to: { name: 'Vegalou' } },
 	{ label: 'Weebsite', to: { name: 'Weebsite' } },
 	{
 		label: 'Razer4Ever',
 		to: { name: 'Razer4Ever' },
+		icon: 'i-simple-icons-razer',
 	},
 	{ label: 'RocketGame', href: 'https://rg.littlebitgay.de' },
 ];
