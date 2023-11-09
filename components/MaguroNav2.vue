ab4939685eb738cbc2be6648de2a24646b9091e1
Oliver Sader <thunfisch987@gmail.com>
2023-11-10 00:11:02 +0100

    :lipstick: [Navbar] fix layout (flex)

--- components/MaguroNav2.vue
+++ components/MaguroNav2.vue
@@ -5,23 +5,20 @@
 				class="flex items-center justify-between p-6 lg:px-8"
 				aria-label="Global"
 			>
-				<div class="flex lg:flex-1">
-					<a href="#" class="-m-1.5 p-1.5">
+				<div class="flex mr-auto">
+					<a href="#">
 						<span class="sr-only">MaguroNetwork</span>
 						<img class="h-8 w-auto" src="/favicon.svg" alt="" />
 					</a>
 				</div>
-				<div class="flex">
 					<UButton
 						color="gray"
 						icon="i-heroicons-bars-3"
 						variant="ghost"
-						size="md"
-						v-if="!mobileMenuOpen"
-						:padded="false"
+						size="xl"
+						class="ml-auto"
 						@click="mobileMenuOpen = true"
 					/>
-				</div>
 			</nav>
 			<MaguroSidebar />
 		</header>
