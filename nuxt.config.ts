85b0dc0b8d7feeb59578cc288743fdb953be9a1d
Oliver Sader <thunfisch987@gmail.com>
2023-12-26 21:37:21 +0100

    :arrow_up::package:update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['@nuxt/ui'],
+	modules: ['@nuxt/ui', 'nuxt-typed-router'],
 	experimental: {
-		typedPages: true,
 		headNext: true,
 		typescriptBundlerResolution: true,
 		writeEarlyHints: true,
