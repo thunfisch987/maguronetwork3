d18884aae35da64a9e24aac0aebad244d4d69a19
thunfisch987 <thunfisch987@gmail.com>
2024-01-21 02:04:48 +0100

    :art: format code

--- app.vue
+++ app.vue
@@ -1,15 +1,15 @@
 <template>
-	<div>
-		<!-- <VitePwaManifest /> -->
-		<NuxtLoadingIndicator :throttle="0" />
-		<NuxtLayout name="default">
-			<NuxtPage />
-		</NuxtLayout>
-	</div>
+	<NuxtLoadingIndicator :throttle="0" />
+	<NuxtLayout name="default">
+		<NuxtPage />
+	</NuxtLayout>
 </template>
 
 <script setup lang="ts">
 useHead({
-	title: useRoute().name === 'index' ? 'MaguroNetwork' : useRoute().name?.toString(),
+	title:
+		useRoute().name === 'index'
+			? 'MaguroNetwork'
+			: useRoute().name?.toString(),
 });
 </script>
