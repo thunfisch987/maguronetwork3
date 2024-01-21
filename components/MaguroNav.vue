23353379f76f5903b9581e60ecd5b7a5f3a230c2
thunfisch987 <thunfisch987@gmail.com>
2024-01-21 02:01:31 +0100

    :art:[Navbar] remove outer div

--- components/MaguroNav.vue
+++ components/MaguroNav.vue
@@ -1,48 +1,46 @@
 <template>
-	<div class="">
-		<header class="absolute inset-x-0 top-0">
-			<nav
-				class="flex items-center justify-between p-6 lg:px-8"
-				aria-label="Global"
-			>
-				<div class="flex mr-auto">
-					<a href="#">
-						<span class="sr-only">MaguroNetwork</span>
-						<UIcon
-							class="h-8 w-auto"
-							name="i-logos-nuxt-icon"
-							dynamic
-						/>
-					</a>
-				</div>
-				<ClientOnly>
-					<UButton
-						:icon="
-							appStore.isDark
-								? 'i-heroicons-moon-20-solid'
-								: 'i-heroicons-sun-20-solid'
-						"
-						color="gray"
-						variant="ghost"
-						aria-label="Theme"
-						class="ml-auto mr-4"
-						@click="appStore.toggleColorMode"
+	<header class="">
+		<nav
+			class="flex items-center justify-between p-6 lg:px-8"
+			aria-label="Global"
+		>
+			<div class="flex mr-auto">
+				<a href="#">
+					<span class="sr-only">MaguroNetwork</span>
+					<UIcon
+						class="h-8 w-auto"
+						name="i-logos-nuxt-icon"
+						dynamic
 					/>
-					<template #fallback>
-						<div class="w-8 h-8"></div>
-					</template>
-				</ClientOnly>
+				</a>
+			</div>
+			<ClientOnly>
 				<UButton
+					:icon="
+						appStore.isDark
+							? 'i-heroicons-moon-20-solid'
+							: 'i-heroicons-sun-20-solid'
+					"
 					color="gray"
-					icon="i-heroicons-bars-3"
 					variant="ghost"
-					size="xl"
-					@click="appStore.toggleSidebar()"
+					aria-label="Theme"
+					class="ml-auto mr-4"
+					@click="appStore.toggleColorMode"
 				/>
-			</nav>
-			<MaguroSidebar />
-		</header>
-	</div>
+				<template #fallback>
+					<div class="w-8 h-8"></div>
+				</template>
+			</ClientOnly>
+			<UButton
+				color="gray"
+				icon="i-heroicons-bars-3"
+				variant="ghost"
+				size="xl"
+				@click="appStore.toggleSidebar()"
+			/>
+		</nav>
+		<MaguroSidebar />
+	</header>
 </template>
 
 <script setup lang="ts">
