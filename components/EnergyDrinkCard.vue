42e95d1a966b96a2eb9c106a1608b7d0761967f6
Oliver Sader <thunfisch987@gmail.com>
2024-02-22 16:49:09 +0100

    :sparkles:add energydrink card

---
+++ components/EnergyDrinkCard.vue
@@ -0,0 +1,29 @@
+<template>
+	<UCard>
+		<template #header>
+			{{ props.brandname }}
+		</template>
+		<template
+			><img
+				:src="props.image"
+				:alt="props.brandname"
+		/></template>
+		<template #footer>
+			<NuxtLink :to="{name: props.link}">{{ props.brandname }}</NuxtLink>
+		</template>
+	</UCard>
+</template>
+
+<script setup lang="ts">
+import type { RoutesNamesList } from '@typed-router/__routes';
+
+
+type EnergyDrinkWikiRoutes = "monster-energy"|"juiced-monster"|"zero-zucker"|"rehab-monster"
+type EnergyDrinkWikiRoutes2 = `EnergyDrinkWiki-${EnergyDrinkWikiRoutes}`
+
+const props = defineProps<{
+	brandname: string;
+	image: string;
+	link: EnergyDrinkWikiRoutes2;
+}>();
+</script>
