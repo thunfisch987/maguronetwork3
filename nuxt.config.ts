3c48c55fbabbe7a3fb8d41d1ed44b0b98ac9a457
Oliver Sader <thunfisch987@gmail.com>
2024-08-28 17:59:52 +0200

    update deps and add nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,7 @@ export default defineNuxtConfig({
         '@nuxtjs/color-mode',
         'unplugin-icons/nuxt',
         '@nuxt/eslint',
+        '@nuxthub/core'
     ],
 	experimental: {
 		headNext: true,
