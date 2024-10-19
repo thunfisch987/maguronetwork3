491bd8fd322b20b137025b6727e1f7d2182f2362
Oliver Sader <thunfisch987@gmail.com>
2024-10-20 01:04:41 +0200

    :wrench::heavy_plus_sign: add eslint-config-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,15 @@
 import withNuxt from './.nuxt/eslint.config.mjs';
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 /**@type {import('@nuxt/eslint-config/flat').FlatConfig[]} */
 export default withNuxt({
+	plugins: [],
 	rules: {
 		'vue/html-indent': 'off',
 		'@typescript-eslint/no-unused-vars': [
 			'warn',
 			{ varsIgnorePattern: '^_|_' },
 		],
+		...eslintConfigPrettier.rules
 	},
 });
